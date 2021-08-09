5568723fc6ee2f6b5d5a3507de9205199b47de2e
Oleg Vaskevich <oleg_vas08@mail.ru>
2021-08-10 00:23:15 +0300

    feat: implement root container, event handlers

--- pages/script.js
+++ pages/script.js
@@ -1,26 +1,32 @@
 
 const select = document.getElementsByTagName('select');
-select[0].addEventListener('click', () => {
-  console.log('click');
+select[0].addEventListener('change', (e) => {
+  console.log(e.target.value);
 })
+
+const getAttribute = (element, attrName) => {
+  if (!element || !attrName) return;
+  const attributes = [...element.attributes];
+  const dataAtr = attributes.find(elem => elem.name === attrName);
+  return !dataAtr ? null : dataAtr.value;
+}
 
 const getDataLevel = (option) => {
   if (!option) return 0;
-  const attributes = [...option.attributes];
-  const dataAtr = attributes.find(elem => elem.name === 'data-level');
-  if (!dataAtr) return null;
-  return dataAtr.value;
+  return getAttribute(option, 'data-level');
 }
 
-
-
-const getSelectOptionsData = (selectOptions) => {
+const getSelectData = (select) => {
+  const selectOptions = [...select];
   const optionsArrayData = [];
+  let selectedItems = 0;
   selectOptions.map((elem, id, arr) => {
+    selectedItems += elem.selected;
     const level = getDataLevel(elem);
     const prevLevel = getDataLevel(arr[id - 1]);
     const obj = {
       option: elem,
+      select,
       level,
       value: elem.value,
       parent: null,
@@ -53,24 +59,43 @@ const getSelectOptionsData = (selectOptions) => {
     optionsArrayData.push(obj);
   });
 
-  return optionsArrayData;
+
+  const selectData = {
+    selectName: select.name,
+    selectedItems,
+  };
+
+  return { optionsArrayData, selectData };
 }
 
-const generateCustomSelect = (optionsArrayData) => {
-  const body = document.body;
-  const wrapperDiv = create('div', 'wrapper', null, body);
-  const customSelectDiv = create('div', 'custom-select', null, wrapperDiv);
+const generateCustomSelect = (optionsArrayData, { selectName, selectedItems }, wrapperDiv) => {
+  
+  const selectDiv = create('div', 'select', null, wrapperDiv);
+  const selectContainerDiv = create('div', 'select-container', null, selectDiv);
+  const titleContainerDiv = create('div', 'title-container', null, selectContainerDiv);
+  const selectTitleP = create('p', 'select__title', null, titleContainerDiv);
+  selectTitleP.innerHTML = selectName;
+  const showSelectedP = create(
+    'p', 
+    selectedItems 
+    ? 'show-selected' 
+    : 'show-selected show-selected--hidden', 
+    null, 
+    titleContainerDiv
+  );
+  showSelectedP.innerHTML = `Показать выбранное (${selectedItems})`;
+  const selectRootDiv = create('div', 'select-root', null, selectContainerDiv);
+  selectRootDiv.innerHTML = 'Код ОКРБ или наименование закупаемой продукции';
+
+  const customSelectDiv = create('div', 'custom-select custom-select--hidden', null, selectDiv);
   const searchContainerDiv = create('div', 'search-container', null, customSelectDiv)
   const searchNavDiv = create('div', 'search__navigation', null, searchContainerDiv);
-  const backImg = create('img', null, null, searchNavDiv, ['src', "../assets/images/ic_backArrow.svg"], ['alt', 'back']);
+  const backImg = create('img', 'back', null, searchNavDiv, ['src', "../assets/images/ic_backArrow.svg"], ['alt', 'back']);
   const searchNavP = create('p', null, null, searchNavDiv);
   searchNavP.innerHTML =  'Реализуемые товары';
-  const searchInput = create('input', 'search__input', null, searchContainerDiv, ['type', 'search'], ['placeholder', 'search']);
-
+  const searchInput = create('input', 'search__input', null, searchContainerDiv, ['type', 'text'], ['placeholder', 'Код ОКРБ или наименование закупаемой продукции']);
   const selectItemsContainerDiv = create('div', 'select-items__container', null, customSelectDiv);
-
   const selectItemsArray = optionsArrayData.map((elem) => {
-    
     const selectItemContainerDiv = create(
       'div', 
       elem.checked 
@@ -97,14 +122,17 @@ const generateCustomSelect = (optionsArrayData) => {
     elem.itemContainer = selectItemContainerDiv;
     return selectItemContainerDiv;
   });
-
   const selectItemsDiv = create('div', 'select-items', selectItemsArray, selectItemsContainerDiv);
   
   const footerDiv = create('div', 'custom-select__footer', null, customSelectDiv);
   create('button', 'footer__button footer__button--primarily', null, footerDiv).innerHTML = 'Применить';
   create('button', 'footer__button footer__button--secondary', null, footerDiv).innerHTML = 'Очистить'
 
-  return { selectItemsDiv }
+  const checkedRoot = selectItemsDiv.querySelector('.select-item__container--checked');
+  if (checkedRoot) selectRootDiv.innerHTML = checkedRoot?.children[1]?.innerText;
+  else selectRootDiv.classList.add('select-root--empty');
+
+  return { selectItemsDiv, selectContainerDiv, customSelectDiv }
 }
 
 const toggleCheckboxAllChildren = (option, isChecked) => {
@@ -133,16 +161,16 @@ const toggleCheckAllParents = (option) => {
   const childrenCount = option.parent.children.length;
   const dotDiv = option.parent.itemContainer.children[0].children[0];
   if (checkCount === childrenCount) {
-    dotDiv.classList.add('checkbox-dot--hidden');
+
     option.parent.checked = !option.parent.checked;
     option.parent.itemContainer.classList.toggle('select-item__container--checked');
     option.parent.itemContainer.children[0]?.classList.toggle('select-item__checkbox--checked');
   } else if (option.parent.checked) {
-    dotDiv.classList.add('checkbox-dot--hidden');
+
     option.parent.checked = !option.parent.checked;
     option.parent.itemContainer.classList.toggle('select-item__container--checked');
     option.parent.itemContainer.children[0]?.classList.toggle('select-item__checkbox--checked');
-  } else dotDiv.classList.remove('checkbox-dot--hidden');
+  } 
   toggleCheckAllParents(option.parent);
 }
 
@@ -193,35 +221,82 @@ const expandHandler = (e, optionsArrayData) => {
   if (option.isExpanded) toggleExpandAllChildren(option, true);
   else toggleExpandAllChildren(option, false);
   option.isExpanded = !option.isExpanded;
-
   arrow.classList.toggle('arrow--closed');
-  console.log(option);
 }
 
-const customSelectEventHandler = (selectItemsDiv, optionsArrayData) => {
-  selectItemsDiv.addEventListener('click', (e) => {
+const backHandler = (e, customSelectDiv) => {
+  const back = e.target.closest('.back');
+  if (!back) return;
+  customSelectDiv.classList.add('custom-select--hidden');
+  
+}
+
+const unCheckAllOptions = ({ selectItemsDiv, optionsArrayData, selectContainerDiv }) => {
+  const [...checkedOptionDivs] = selectItemsDiv.querySelectorAll('.select-item__container--checked');
+  const checkedValues = checkedOptionDivs.map((elem) => {
+    elem.children[0].classList.remove('select-item__checkbox--checked');
+    elem.classList.remove('select-item__container--checked');
+    return getAttribute(elem, 'value');
+  });
+  optionsArrayData.filter((elem) => checkedValues.includes(elem.value)).map((el) => {
+    optionsArrayData[0].select.dispatchEvent(new Event('change'));
+    el.checked = false;
+    el.option.removeAttribute('selected');
+  });
+  
+  selectContainerDiv.children[1].innerHTML = 'Код ОКРБ или наименование закупаемой продукции';
+  selectContainerDiv.children[1].classList.add('select-root--empty');
+  selectContainerDiv.children[0].children[1].classList.add('show-selected--hidden');
+}
+
+const clearButtonHandler = (e, args) => {
+  const clearButton = e.target.closest('.footer__button--secondary');
+  if (!clearButton) return;
+  unCheckAllOptions(args);
+}
+
+const submitButtonHandler = (e, customSelectDiv, selectItemsDiv, selectContainerDiv) => {
+  const submitButton = e.target.closest('.footer__button--primarily');
+  if (!submitButton) return;
+  const checkedRoot = selectItemsDiv.querySelectorAll('.select-item__container--checked');
+  if (!checkedRoot.length) return;
+  selectContainerDiv.children[1].innerHTML = checkedRoot[0]?.children[1]?.innerText;
+  selectContainerDiv.children[1].classList.remove('select-root--empty');
+  selectContainerDiv.children[0].children[1].classList.remove('show-selected--hidden');
+  selectContainerDiv.children[0].children[1].innerHTML = `Показать выбранное (${checkedRoot.length})`
+  customSelectDiv.classList.add('custom-select--hidden');
+}
+
+const customSelectEventHandler = (customSelectDiv, selectItemsDiv, selectContainerDiv, optionsArrayData) => {
+  customSelectDiv.addEventListener('click', (e) => {
     checkboxHandler(e, optionsArrayData);
     expandHandler(e, optionsArrayData);
+    backHandler(e, customSelectDiv);
+    clearButtonHandler(e, { selectItemsDiv, optionsArrayData, selectContainerDiv });
+    submitButtonHandler(e, customSelectDiv, selectItemsDiv, selectContainerDiv);
+  })
+}
+
+const selectContainerEventHandler = (selectContainerDiv, customSelectDiv) => {
+  selectContainerDiv.addEventListener('click', (e) => {
+    const selectRoot = e.target.closest('.select-root');
+    const showSelected = e.target.closest('.show-selected');
+    if (!selectRoot && !showSelected) return;
+    customSelectDiv.classList.remove('custom-select--hidden');
   })
 }
 
 
 const customeSelect = () => {
   const selectElements = [...document.getElementsByTagName('select')];
+  const { body } = document;
+  const wrapperDiv = create('div', 'wrapper', null, body);
   selectElements.forEach((elem) => {
-    const selectOptions = [...elem];
-    const optionsArrayData = getSelectOptionsData(selectOptions)
-    const { selectItemsDiv } = generateCustomSelect(optionsArrayData);
-    
-    customSelectEventHandler(selectItemsDiv, optionsArrayData);
+    const { optionsArrayData, selectData } = getSelectData(elem);
+    const { selectItemsDiv, selectContainerDiv, customSelectDiv } = generateCustomSelect(optionsArrayData, selectData, wrapperDiv);
+    customSelectEventHandler(customSelectDiv, selectItemsDiv, selectContainerDiv, optionsArrayData);
+    selectContainerEventHandler(selectContainerDiv, customSelectDiv);
   })
-  // const selectOptions = [...selectElements[0]];
-  // const optionsArrayData = getSelectOptionsData(selectOptions)
-  // const { selectItemsDiv } = generateCustomSelect(optionsArrayData);
-  
-  // customSelectEventHandler(selectItemsDiv, optionsArrayData);
-  
-  // console.log(optionsArrayData);
 }
 
 
@@ -265,5 +340,13 @@ function create(el, classNames, child, parent, ...dataAttr) {
     });
   }
   return elem;
+}
+
+
+function Event( event, params ) {
+  params = params || { bubbles: false, cancelable: false, detail: undefined };
+  var evt = document.createEvent('CustomEvent');
+  evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+  return evt;
 }
 
